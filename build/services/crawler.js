07040099ab6abc9cbfcfe6eee8abd60541ffb4ea
Andre Suzanovich <supius@gmail.com>
2021-07-24 14:16:28 +0200

    add error handling

--- build/services/crawler.js
+++ build/services/crawler.js
@@ -16,19 +16,24 @@ class Crawler {
         return res;
     }
     async _crawl(url, visitedUrls, res) {
-        if (visitedUrls.has(url))
-            return;
-        visitedUrls.add(url);
-        const content = await this._contentDownloader.getContent(url);
-        const links = await this._contentParser.getLinks(content);
-        res.push({ url, links });
-        console.log('\x1b[32m%s\x1b[0m', `Processing URL: '${url}'`);
-        const baseURLObj = new url_1.URL(url);
-        const urls = await this._contentParser.getURLs(`${baseURLObj.protocol}//${baseURLObj.hostname}`, content);
-        for (const urlDoc of urls) {
-            if (new url_1.URL(urlDoc).hostname != this._baseHostname)
-                continue;
-            await this._crawl(urlDoc, visitedUrls, res);
+        try {
+            if (visitedUrls.has(url))
+                return;
+            visitedUrls.add(url);
+            const content = await this._contentDownloader.getContent(url);
+            const links = await this._contentParser.getLinks(content);
+            res.push({ url, links });
+            console.log('\x1b[32m%s\x1b[0m', `Processing URL: '${url}'`);
+            const baseURLObj = new url_1.URL(url);
+            const urls = await this._contentParser.getURLs(`${baseURLObj.protocol}//${baseURLObj.hostname}`, content);
+            for (const urlDoc of urls) {
+                if (new url_1.URL(urlDoc).hostname != this._baseHostname)
+                    continue;
+                await this._crawl(urlDoc, visitedUrls, res);
+            }
+        }
+        catch (err) {
+            console.log('\x1b[31m%s\x1b[0m', `Error crawling URL: '${url}', Error: ${JSON.stringify(err)}`);
         }
     }
 }
--- src/services/crawler.ts
+++ src/services/crawler.ts
@@ -40,23 +40,27 @@ export class Crawler implements ICrawler {
     }
 
     private async _crawl(url: string, visitedUrls: Set<string>, res: CrawlerURLResult[]): Promise<void> {
-        if (visitedUrls.has(url)) return;
+        try {
+            if (visitedUrls.has(url)) return;
 
-        visitedUrls.add(url);
+            visitedUrls.add(url);
 
-        const content = await this._contentDownloader.getContent(url);
-        const links = await this._contentParser.getLinks(content);
-        
-        res.push({ url, links});
-        console.log('\x1b[32m%s\x1b[0m', `Processing URL: '${url}'`);
+            const content = await this._contentDownloader.getContent(url);
+            const links = await this._contentParser.getLinks(content);
+            
+            res.push({ url, links});
+            console.log('\x1b[32m%s\x1b[0m', `Processing URL: '${url}'`);
 
-        const baseURLObj = new URL(url);        
-        const urls = await this._contentParser.getURLs(`${baseURLObj.protocol}//${baseURLObj.hostname}`, content);
+            const baseURLObj = new URL(url);        
+            const urls = await this._contentParser.getURLs(`${baseURLObj.protocol}//${baseURLObj.hostname}`, content);
 
-        for (const urlDoc of urls) {
-            if (new URL(urlDoc).hostname != this._baseHostname) continue;
+            for (const urlDoc of urls) {
+                if (new URL(urlDoc).hostname != this._baseHostname) continue;
 
-            await this._crawl(urlDoc, visitedUrls, res);
-        }
+                await this._crawl(urlDoc, visitedUrls, res);
+            }
+        } catch (err) {
+            console.log('\x1b[31m%s\x1b[0m', `Error crawling URL: '${url}', Error: ${JSON.stringify(err)}`);
+        }        
     }
 }
